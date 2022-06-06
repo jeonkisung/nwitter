c0c7b00e50686a3200b4530edc8114f179c71fcb
전기성 <99243495+jeonkisung@users.noreply.github.com>
2022-06-06 14:55:31 +0900

    0606/42

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDoOEc92D3CC7RLN5uEH8bpjIjCoQ7_aZQ",
+    authDomain: "nwitter-1f0de.firebaseapp.com",
+    projectId: "nwitter-1f0de",
+    storageBucket: "nwitter-1f0de.appspot.com",
+    messagingSenderId: "99133184683",
+    appId: "1:99133184683:web:51c674670c73a6ad4a75ee"
+  };
+
+ export default firebase.initializeApp(firebaseConfig);
